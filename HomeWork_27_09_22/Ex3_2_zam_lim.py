771ccf7c1158ef69507a78733d25e7b6ff052fe8
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-29 01:36:20 +0500

    add Ex3

--- HomeWork_27_09_22/Ex3_2_zam_lim.py
+++ HomeWork_27_09_22/Ex3_2_zam_lim.py
@@ -1,12 +1,21 @@
 # Ex3_2_zam_lim. Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
 # Пример:
-# - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
-from os import sep
+# - Для n = 5: сумма = 11,55
+
 n = int(input("Введите число: "))
-array_2_zam_lim = [0]*n
 sum_num_array = 0
 for i in range(1, n + 1):
-    array_2_zam_lim[i - 1] = (1 + 1 / i ) ** i
-    sum_num_array += array_2_zam_lim[i - 1]
-print(array_2_zam_lim)
-print(sum_num_array)
+    sum_num_array += (1 + 1 / i ) ** i
+print(f'Для n = {n}: сумма = {round(sum_num_array, 2)}')
+
+
+
+# n = int(input("Введите число: "))
+# array_2_zam_lim = [0]*n
+# sum_num_array = 0
+# for i in range(1, n + 1):
+#     array_2_zam_lim[i - 1] = (1 + 1 / i ) ** i
+#     sum_num_array += array_2_zam_lim[i - 1]
+# print(array_2_zam_lim)
+# print(f'Для n = {n}: сумма = {sum_num_array}')
+
