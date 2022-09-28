5a544d7f6a6bced43fa2fea6502105a4174c95ec
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-29 01:24:39 +0500

    update Ex2

--- HomeWork_27_09_22/Ex2_array_mult_num_N.py
+++ HomeWork_27_09_22/Ex2_array_mult_num_N.py
@@ -1,10 +1,20 @@
 # Ex2_array_mult_num_N. Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4) 
-from os import sep
+
 n = int(input("Введите число: "))
 array_mult_num = [0]*n
 array_mult_num[0] = 1
 for i in range(1, n):
     array_mult_num[i] = array_mult_num[i - 1] * (i + 1)
-print(array_mult_num, sep = ',')
+print(array_mult_num)
+
+
+
+# def factorial(n):
+#     res = 1
+#     for k in range(1, n + 1):
+#        res *= k
+#     return res
+# for i in range(1, n + 1):
+#     print(factorial(i), end=' ')
