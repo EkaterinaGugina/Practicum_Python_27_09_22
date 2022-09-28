3fd976941ebf9158e7ca8e3be13604005899ec22
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-29 00:38:42 +0500

    commit Ex3__Work_Klass

--- WorkPracticum/Ex3.py
+++ WorkPracticum/Ex3.py
@@ -1,20 +1,12 @@
 # Напишите программу, которая принимает на вход число N и выдаёт последовательность из N членов.
 #  Пример:
 # Для N = 5: 1, -3, 9, -27, 81
-from tkinter import N
-
 
 n = int(input("Введите число: "))
-# lst = [0]*n
-# lst[0] = 1
-# for i in range(1, n):
-#     lst[i] = lst[i - 1]*(-3)
+num = 1
+for i in range(num):
+    print(num, end=" ")
+    num *= -3
     
 # for i in range(5):
 #     print((-3) ** i, end=" ")
-
-number = int(input("Введите число: "))
-num = 1
-for i in range(number):
-    print(num, end=" ")
-    num *= -3
