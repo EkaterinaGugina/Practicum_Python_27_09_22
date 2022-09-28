c33e2384bcf744b4a55c11fb8e04d9ca2c024f39
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-29 02:49:37 +0500

    HW solved

--- HomeWork_27_09_22/Ex4_mult_condition_file.py
+++ HomeWork_27_09_22/Ex4_mult_condition_file.py
@@ -3,23 +3,9 @@
 n = int(input("Введите число: "))
 array_mult_num = [i for i in range(-n, n + 1)]
 print(array_mult_num)
-f = open('file.txt', 'r')
-array_index = f.readline()
-print(*f)
+mult_num_index = 1
+with open("file.txt", "r") as f:
+    for line in f:
+        mult_num_index *= array_mult_num[int(line)]
+print(mult_num_index)
 f.close
-# mult_num_index = 1
-# for i in array_index:
-#     mult_num_index *= array_mult_num[int(i)]
-    
-    
-    
-#     n = int(input("Введите число: "))
-# array_mult_num = [i for i in range(-n, n + 1)]
-# print(array_mult_num)
-# f = open('C:\Users\ЭкзаменСдам\Desktop\GeekBrains\Python\Practicum_Python_27_09_22\HomeWork_27_09_22/file.txt', 'r')
-# array_index = (f.readline())
-# print(array_index)
-# f.close
-# mult_num_index = 1
-# for i in array_index:
-#     mult_num_index *= array_mult_num[int(i)]
