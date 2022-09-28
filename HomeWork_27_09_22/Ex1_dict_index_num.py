96cc9494bb53667afaa7ce76f411f686aab12d0a
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-29 00:56:47 +0500

    add folder HomeWork 27_09

--- HomeWork_27_09_22/Ex1_dict_index_num.py
+++ HomeWork_27_09_22/Ex1_dict_index_num.py
@@ -2,8 +2,6 @@
 # Пример:
 # Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
-n = input("Введите число: ")
-for i in range(n):
-    
-        
-print('Сумма цифр, входящих в это число равна ', sum_num_real)
+n = int(input("Введите число: "))
+dictioner_index = {i: 3 * i + 1 for i in range(1, n+1)}
+print(dictioner_index)
