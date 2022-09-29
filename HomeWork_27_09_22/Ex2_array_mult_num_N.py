52384d7a9326e12c4461b40aeda5e25a175f0ccb
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-30 00:05:14 +0500

    update Ex2 is recursia

--- HomeWork_27_09_22/Ex2_array_mult_num_N.py
+++ HomeWork_27_09_22/Ex2_array_mult_num_N.py
@@ -2,19 +2,17 @@
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4) 
 
-n = int(input("Введите число: "))
-array_mult_num = [0]*n
-array_mult_num[0] = 1
-for i in range(1, n):
-    array_mult_num[i] = array_mult_num[i - 1] * (i + 1)
-print(array_mult_num)
+def factorial(k):
+    if k == 1: return 1
+    else:
+        return k * factorial(k-1)
+n = int(input('введите число n = '))
+array = [factorial(i) for i in range(1, n+1)]
+print(array)
 
-
-
-# def factorial(n):
-#     res = 1
-#     for k in range(1, n + 1):
-#        res *= k
-#     return res
-# for i in range(1, n + 1):
-#     print(factorial(i), end=' ')
+# n = int(input("Введите число: "))
+# array_mult_num = [0]*n
+# array_mult_num[0] = 1
+# for i in range(1, n):
+#     array_mult_num[i] = array_mult_num[i - 1] * (i + 1)
+# print(array_mult_num)
