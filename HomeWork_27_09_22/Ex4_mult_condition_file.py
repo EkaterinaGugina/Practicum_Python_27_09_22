0393a0eacfef5d33afce11f364d0bc84367f1223
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-10-04 23:56:46 +0500

    update Ex5

--- HomeWork_27_09_22/Ex4_mult_condition_file.py
+++ HomeWork_27_09_22/Ex4_mult_condition_file.py
@@ -9,3 +9,38 @@ with open("file.txt", "r") as f:
         mult_num_index *= array_mult_num[int(line)] 
 print(mult_num_index)     # В file.txt записаны индексы 2, 1, 6
 f.close
+
+
+pos = int(input("Введите целое число больше 4: "))
+addres = 'spisok.txt'
+
+# Создание списка от -N до N
+def Spisok (number):
+    list = []
+    for i in range(-number, number+1):
+        list.append(i)
+    return list
+
+# # Создание списка из строки в файле
+# def FileList(addresFile):
+#     file = open(addresFile, 'r')
+#     list = file.read()
+#     file.close()
+#     list = list.replace(" ", "")
+#     list = list.split(",")
+#     for i in range(len(list)):
+#         list[i] = int(list[i])
+#     return list
+
+# list = Spisok(pos)
+# sp = FileList(addres)
+# print(list)
+# print(sp)
+# listX = 1
+# for i in sp:
+#     listX*=list[i]
+
+# print(listX)
+    
+# # 2        ,4,     0,  10
+# # 2,4,0,10
--- HomeWork_27_09_22/Ex5_spisok_random.py
+++ HomeWork_27_09_22/Ex5_spisok_random.py
@@ -1,17 +1,41 @@
 #  Написать программу перемешивающую список
-from cmath import exp
 import datetime
 from random import randrange
+from time import time
 n = int(input('Введите количество элементов n = '))
 array = [randrange(-99, 100) for i in range(n)]
 print(array)
 for i in range(len(array)):
     current = array[i]
     k = datetime.datetime.now().microsecond % n
-    # print(k)
+    print(k)
+    time.sleep(k*0.05)
     array[i] = array[k]
     array[k] = current 
 print(array)
+
+
+
+
+# def getRandomInt(randomRange):
+#     return datetime.datetime.now().microsecond % randomRange
+
+
+# def shuffleList(list):
+#     shuffledLst = []
+#     while (len(list) != 0):
+#         index = getRandomInt(len(list))
+#         el = list[index]
+#         shuffledLst.append(el)
+#         list.remove(el)
+#     return shuffledLst
+# import util
+
+# baseLst = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+# print(baseLst)
+# shuffledLst = util.shuffleList(baseLst)
+# print(baseLst)
+# print(shuffledLst)
 
     
 
